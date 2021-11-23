1588f4f270f09a2e0892fcd18be29877bfffc5df
Ahmed Banna <ahmedbanna6@gmail.com>
2021-11-23 19:24:44 +0200

    edit learn route

---
+++ client/src/data/learn.js
@@ -0,0 +1,34 @@
+const learn = [
+	{
+		id: 1,
+		imgUrl: 'https://source.unsplash.com/BTKF6G-O8fU/1600x1100',
+		subHeading: 'Step. 01',
+		heading: 'Property listed for sale',
+		body: '',
+		isDisabled: true,
+		link: '',
+		anchor: '',
+	},
+	{
+		id: 2,
+		imgUrl: 'https://source.unsplash.com/9opiHRPIvR0/1600x900',
+		subHeading: 'Step. 02',
+		heading: 'Property is divided into shares',
+		body: '',
+		isDisabled: true,
+		link: '',
+		anchor: '',
+	},
+	{
+		id: 3,
+		imgUrl: 'https://source.unsplash.com/ztYmIQecyH4/1600x900',
+		subHeading: 'Step. 03',
+		heading: 'People buy shares ',
+		body: '',
+		isDisabled: true,
+		link: '',
+		anchor: '',
+	},
+];
+
+export default learn;
--- client/src/routes/Learn/index.js
+++ client/src/routes/Learn/index.js
@@ -1,45 +1,19 @@
-import React from 'react';
+import { Page, Footer } from '../../components';
+import learn from '../../data/learn';
 import './learn.css';
 
 const Learn = () => {
 	return (
 		<div className='learn-scroll'>
 			<div className='learn'>
-				<div className='learn-section'>
-					<h2>How it works</h2>
-					<p>
-						1. Real estate agents or property owners lists properties for sale
-					</p>
-					<p>
-						2. l.dorado divide each property into million shares for initial
-						public offering
-					</p>
-					<p>
-						3. Individual investors can buy as many shares they want from the
-						initial million shares
-					</p>
-					<p>
-						4. After all the initial shares is sold out a contract is signed
-						with the property owner to get the money then the property is rented
-						and the rent is distributed to the shareholders
-					</p>
-				</div>
-				<div className='learn-section'>
-					<h2>How to invest</h2>
-					<p>1. Go to invest tab</p>
-					<p>2. Select which property to invest in</p>
-					<p>3. Add the amount of shares you want to buy then click invest</p>
-					<p>
-						Quick Tip: buy shares in the initial public offering and hold them
-						to all shares is sold out and the property is rent then you can sell
-						those shares at a higher price or you can hold them and earn a
-						passive income from the monthly rent and benefit from the property
-						appreciation
-					</p>
-				</div>
+				{learn.map((content, index) => (
+					<div key={index}>
+						<Page page={content} />
+					</div>
+				))}
 			</div>
+			<Footer />
 		</div>
 	);
 };
-
 export default Learn;
